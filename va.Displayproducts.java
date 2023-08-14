d2b234b027078e4912f9e8359762f7b3d48f2668
yesu <119784057+yesubabu7@users.noreply.github.com>
2023-08-14 17:36:43 +0530

    Update va.Displayproducts.java

--- va.Displayproducts.java
+++ va.Displayproducts.java
@@ -59,3 +59,66 @@ public class DisplayProducts extends HttpServlet {
 		out.println("</html>");
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+package demo;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/CartServlet")
+public class CartServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		@SuppressWarnings("unchecked")
+		List<String> cartItems = (List<String>) session.getAttribute("cartItems");
+
+		if (cartItems == null) {
+			cartItems = new ArrayList<>();
+			session.setAttribute("cartItems", cartItems);
+		}
+
+		String action = request.getParameter("action");
+		String product = request.getParameter("product");
+
+		if (action != null) {
+			if (action.equals("add")) {
+				if (product != null && !product.isEmpty()) {
+					cartItems.add(product);
+				}
+			} else if (action.equals("remove")) {
+				if (product != null && !product.isEmpty()) {
+					cartItems.remove(product);
+				}
+			}
+		}
+
+		response.sendRedirect("yh.html"); // Redirect back to the product page
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+}
